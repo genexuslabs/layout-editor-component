f5d84909bc19a3482754d5353a323c70184dfd8c
Nicolás Cardelino <ncardeli@gmail.com>
2018-11-29 18:10:40 -0300

    Improve simple grid render
    Use the data resolver for rendering the items.

--- src/components/layout-editor/control-resolvers/simple-grid-resolver.tsx
+++ src/components/layout-editor/control-resolvers/simple-grid-resolver.tsx
@@ -2,6 +2,7 @@ import {
   IResolverContext,
   isCellSelected
 } from "../layout-editor-control-resolver";
+import dataResolver from "./data-resolver";
 
 export default function simpleGridResolver(
   { simplegrid },
@@ -10,14 +11,16 @@ export default function simpleGridResolver(
   const headerBarText = simplegrid.controlType || "Grid";
   return (
     <div
-      data-gx-le-control-id={simplegrid["@id"]}
       data-gx-le-control-header-bar
       style={{
         "--gx-le-control-header-bar-text": `'${headerBarText}'`,
         border: "1px solid var(--gx-le-control-header-bar-background-color)"
       }}
+      data-gx-le-simple-grid
     >
       <table
+        data-gx-le-container
+        data-gx-le-control-id={simplegrid["@id"]}
         class={simplegrid["@class"]}
         style={{
           borderCollapse: "collapse"
@@ -37,30 +40,22 @@ export default function simpleGridResolver(
             ))}
           </tr>
         </thead>
-        <tbody>{getGridRows(simplegrid.item, context)}</tbody>
+        <tbody>
+          <tr>
+            {simplegrid.item.map(item => (
+              <td
+                data-gx-le-control-id={item["@id"]}
+                data-gx-le-selected={isCellSelected(item, context).toString()}
+                data-gx-le-drop-area="horizontal"
+              >
+                {dataResolver({
+                  data: item
+                })}
+              </td>
+            ))}
+          </tr>
+        </tbody>
       </table>
     </div>
   );
-}
-
-const MAX_FAKE_ROWS = 5;
-function getGridRows(gridColumns, context) {
-  const rows = [];
-  for (let i = 0; i < MAX_FAKE_ROWS; i++) {
-    rows.push(
-      <tr>
-        {gridColumns.map(item => (
-          <td
-            class={item["@class"]}
-            data-gx-le-control-id={item["@id"]}
-            data-gx-le-selected={isCellSelected(item, context).toString()}
-          >
-            {item["@attribute"]}
-          </td>
-        ))}
-      </tr>
-    );
-  }
-
-  return rows;
 }
