50589a33caf7b568b2e02c60ec887f91f623af81
Daniel Mariño <49991370+dangmarm@users.noreply.github.com>
2019-08-06 11:27:00 -0300

    Fix: Remember selected tab after model update (#5)

--- src/components/layout-editor-tab/layout-editor-tab.tsx
+++ src/components/layout-editor-tab/layout-editor-tab.tsx
@@ -1,8 +1,10 @@
-import { Component, Element, Prop } from "@stencil/core";
+import { Component, Element, Listen, Prop } from "@stencil/core";
 import {
   IResolverContext,
   controlResolver
 } from "../layout-editor/layout-editor-control-resolver";
+
+import { getControlId } from "../layout-editor/layout-editor-helpers";
 
 @Component({
   shadow: false,
@@ -15,34 +17,55 @@ export class LayoutEditorTab {
   @Prop() context: IResolverContext;
   @Prop() model: GeneXusAbstractLayout.Cell;
 
+  tabItemSelectedId = "";
+
+  @Listen("onTabChange")
+  onTabChangeHandler(event: CustomEvent) {
+    this.tabItemSelectedId = getControlId(event.detail.target);
+  }
+
   render() {
     const { tab } = this.model;
 
     this.element.setAttribute("data-gx-le-control-id", tab["@id"] || "");
 
-    return renderTab(this.context, tab);
+    return this.renderTab(this.context, tab);
   }
-}
 
-function renderTab(context: IResolverContext, tab: GeneXusAbstractLayout.Tab) {
-  return (
-    <gx-tab data-gx-le-control-id={tab["@id"]}>
-      {tab.item.map((tabItem, index) => {
-        return renderTabItem(context, tabItem, index === 0);
-      })}
-    </gx-tab>
-  );
-}
+  renderTab(context: IResolverContext, tab: GeneXusAbstractLayout.Tab) {
+    const tabItemSelectedIndex = tab.item.findIndex(
+      el => el["@id"] === this.tabItemSelectedId
+    );
 
-function renderTabItem(
-  context: IResolverContext,
-  tabItem: GeneXusAbstractLayout.TabItem,
-  selected: boolean
-) {
-  return [
-    <gx-tab-caption slot="caption" selected={selected}>
-      {tabItem["@caption"]}
-    </gx-tab-caption>,
-    <gx-tab-page slot="page">{controlResolver(tabItem, context)}</gx-tab-page>
-  ];
+    return (
+      <gx-tab data-gx-le-control-id={tab["@id"]}>
+        {tab.item.map((tabItem, index) => {
+          return this.renderTabItem(
+            context,
+            tabItem,
+            tabItemSelectedIndex === -1
+              ? index === 0
+              : index === tabItemSelectedIndex
+          );
+        })}
+      </gx-tab>
+    );
+  }
+
+  renderTabItem(
+    context: IResolverContext,
+    tabItem: GeneXusAbstractLayout.TabItem,
+    selected: boolean
+  ) {
+    return [
+      <gx-tab-caption
+        slot="caption"
+        data-gx-le-control-id={tabItem["@id"]}
+        selected={selected}
+      >
+        {tabItem["@caption"]}
+      </gx-tab-caption>,
+      <gx-tab-page slot="page">{controlResolver(tabItem, context)}</gx-tab-page>
+    ];
+  }
 }
